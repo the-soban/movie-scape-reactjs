1f677660c13478640b99e618a6def449e147a39d
the-soban <sobanshafeq123@gmail.com>
2023-08-04 22:06:50 +0500

    added routes in App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import Home from "./Home";
 import Search from "./Search";
+import { Link, Route, Routes } from 'react-router-dom';
 
 const App = () => {
     return (
-        <Home />
+        <>
+            <nav>
+                <li><Link to="/">Home Page</Link></li>
+                <li><Link to="/search">Search Page</Link></li>
+            </nav>
+            {/* <Home /> */}
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/search" element={<Search />} />
+            </Routes>
+        </>
     );
 }
 
