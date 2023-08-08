e5727a5c1bcc52a4dacae47e80e5dcd891224d14
the-soban <sobanshafeq123@gmail.com>
2023-08-08 23:36:09 +0500

    theme switch functionality

--- src/App.js
+++ src/App.js
@@ -2,13 +2,22 @@ import Home from "./Home";
 import Search from "./Search";
 import About from "./About";
 import { Link, Route, Routes } from 'react-router-dom';
+import { useState } from "react";
 
 const App = () => {
+    const [light, setLight] = useState(false);
+
+    const handleClick = () => {
+        setLight(!light);
+        document.body.classList.toggle('light-body')
+    }
+
     return (
         <>
             <nav>
                 <Link className="link" to="/"><h1>Moviescape</h1></Link>
                 <ul>
+                    <li className="theme-btn" onClick={handleClick}>Dark Mode</li>
                     <li><Link className="link" to="/">Home Page</Link></li>
                     <li><Link className="link" to="/search">Search Page</Link></li>
                     <li><Link className="link" to="/about">About</Link></li>
