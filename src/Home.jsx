22adb0c535fccda3b4c47ba3b38f5c12418b6a6e
the-soban <sobanshafeq123@gmail.com>
2023-08-09 21:16:04 +0500

    added hero text

--- src/Home.jsx
+++ src/Home.jsx
@@ -4,7 +4,13 @@ const Home = () => {
     return (
         <>
             <main>
-                <h2>Search for any Movies</h2>
+                <h2>Discover the World of Movies with Our App! 🎬🍿</h2>
+                <p>
+                    Whether you're a die-hard cinephile or just looking for a
+                    cozy movie night, we've got you covered. Introducing the
+                    easiest way to explore, search, and dive into the
+                    captivating world of cinema.
+                </p>
             </main>
         </>
     )
